54288594d68faafa1dbeb3f2a62163388c27eae4
chuchu02 <rlaxogns73@gmail.com>
2022-06-06 23:17:34 +0900

    블로그포스트페이지에서 입력된 슬러그를 바탕으로 올바른 포스트내용 불러와서 표시

--- src/pages/blog/{mdx.slug}.js
+++ src/pages/blog/{mdx.slug}.js
@@ -1,12 +1,29 @@
 import React from "react";
-import Layout from '../components/layout';
+import { graphql } from "gatsby";
+import { MDXRenderer } from "gatsby-plugin-mdx";
+import Layout from '../../components/layout';
 
-const BlogPost = () => {
+const BlogPost = ({ data }) => {
   return (
     <Layout pageTitle="Super Cool Blog Posts">
-      <p>My blog post contents will Go here (eventually).</p>
+      <p>{data.mdx.frontmatter.date}</p>
+      <MDXRenderer>
+        {data.mdx.body}
+      </MDXRenderer>
     </Layout>
   )
 };
 
+
+export const query = graphql`
+  query ($id: String) {
+    mdx(id: {eq: $id}) {
+      frontmatter {
+        title
+        date(formatString: "MMMM D, YYYY")
+      }
+      body
+    }
+  }
+`
 export default BlogPost;
