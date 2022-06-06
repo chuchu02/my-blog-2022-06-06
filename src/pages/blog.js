ecd033e519e3d236d42281363c25451a7f57a200
chuchu02 <rlaxogns73@gmail.com>
2022-06-06 20:26:40 +0900

    블로그 페이지 추가

---
+++ src/pages/blog.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../components/layout";
+
+const BlogPage = () => {
+  return (
+    <Layout pageTitle="My Blog Posts">
+      <p>My Cool posts will Go in here</p>
+    </Layout>
+  )
+}
+
+export default BlogPage;
