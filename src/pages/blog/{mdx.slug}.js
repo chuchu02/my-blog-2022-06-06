c6d5733c09cd5605bb236fad903e468273d1398c
chuchu02 <rlaxogns73@gmail.com>
2022-06-06 23:58:16 +0900

    GatsbyImage 컴포넌트로 동적 이미지 노출

--- src/pages/blog/{mdx.slug}.js
+++ src/pages/blog/{mdx.slug}.js
@@ -1,29 +1,46 @@
 import React from "react";
 import { graphql } from "gatsby";
 import { MDXRenderer } from "gatsby-plugin-mdx";
-import Layout from '../../components/layout';
-
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
+import Layout from "../../components/layout";
 const BlogPost = ({ data }) => {
+  const image = getImage(data.mdx.frontmatter.hero_image);
+
   return (
-    <Layout pageTitle="Super Cool Blog Posts">
+    <Layout pageTitle={data.mdx.frontmatter.title}>
       <p>{data.mdx.frontmatter.date}</p>
-      <MDXRenderer>
-        {data.mdx.body}
-      </MDXRenderer>
+      <GatsbyImage
+        image={image}
+        alt={data.mdx.frontmatter.hero_image_alt}
+      />
+      <p>
+        Photo Credit:{" "}
+        <a href={data.mdx.frontmatter.hero_image_credit_link}>
+          {data.mdx.frontmatter.hero_image_credit_text}
+        </a>
+      </p>
+      <hr />
+      <MDXRenderer>{data.mdx.body}</MDXRenderer>
     </Layout>
-  )
+  );
 };
-
-
 export const query = graphql`
   query ($id: String) {
-    mdx(id: {eq: $id}) {
+    mdx(id: { eq: $id }) {
       frontmatter {
         title
-        date(formatString: "MMMM D, YYYY")
+        date(formatString: "MMMM DD, YYYY")
+        hero_image_alt
+        hero_image_credit_link
+        hero_image_credit_text
+        hero_image {
+          childImageSharp {
+            gatsbyImageData
+          }
+        }
       }
       body
     }
   }
-`
+`;
 export default BlogPost;
